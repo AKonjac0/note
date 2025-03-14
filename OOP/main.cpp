e0b1d5ff96b0c63a53a55fef4daf5f8a3f89d037
AKonjac0 <yyr0419@outlook.com>
2025-03-14 18:33:42 +0800

    Deployed  with MkDocs version: 1.6.1

---
+++ OOP/main.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    const char* s = "CCB";
+    const char* p = "CCB";
+    cout << (void*) s << " " << (void*) p << endl;
+    return 0;
+}

de3d18be10bc7d175efc430769fc87298bdefbb1
AKonjac0 <yyr0419@outlook.com>
2025-04-28 17:06:37 +0800

    Deployed  with MkDocs version: 1.6.1

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,8 +1,43 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main() {
-    const char* s = "CCB";
-    const char* p = "CCB";
-    cout << (void*) s << " " << (void*) p << endl;
+
+class base {
+public:
+    base() {
+        int *b = new int[5];
+        cout << "base constructor" << endl;
+        
+    }
+    virtual ~base() {
+        delete[] b;
+        cout << "base destructor" << endl;
+        
+    }
+
+private:
+    int *b;
+};
+
+class derived : public base {
+public:
+    derived() {
+        int *d = new int[8];
+        cout << "derived constructor" << endl;
+    }
+    ~derived() {
+        delete[] d;
+        cout << "derived destructor" << endl;
+    }
+
+private:
+    int *d;
+};
+
+int main()
+{
+    base *pBase = new derived;
+    cout << "---" << endl;
+    delete pBase;
+
     return 0;
 }

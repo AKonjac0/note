c41f93cdb2245c88064bc05d62f5e7bc3fad203e
AKonjac0 <yyr0419@outlook.com>
2025-05-26 15:04:09 +0800

    Deployed  with MkDocs version: 1.6.1

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -4,12 +4,18 @@ using namespace std;
 int main()
 {
     int a = 10;
-    int b = std::move(a);
-    cout << "a: " << a << endl; 
-    cout << "b: " << b << endl; 
-    string x = "hello";
-    string y = std::move(x);
-    cout << "x: " << x << endl;
-    cout << "y: " << y << endl;
+    double *p;
+    int *q = &a; // Pointer to int
+    p = (double *)&a; // Unsafe cast from int* to double*
+    cout << *p << endl;
+    p = reinterpret_cast<double *>(&a); // Unsafe cast using reinterpret_cast
+    cout << *p << endl;
+    cout << *q << endl; // Output the value of a using int pointer
+    ++q;
+    cout << *q << endl;
+
+    double b;
+    memcpy(&b, &a, 8); // Unsafe copy of int to double
+    cout << b << endl; // Output the value of b, which is now undefined
     return 0;
 }
